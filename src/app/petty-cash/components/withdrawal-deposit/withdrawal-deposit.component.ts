b7cf1ca87dbd51078f4681c138bba0e39bd318a5
sadik-walkover <sadik@walkover.in>
2019-10-11 18:15:57 +0530

    pettycash issue fixed

--- src/app/petty-cash/components/withdrawal-deposit/withdrawal-deposit.component.ts
+++ src/app/petty-cash/components/withdrawal-deposit/withdrawal-deposit.component.ts
@@ -1,4 +1,11 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, Inject } from '@angular/core';
+import { watch } from 'fs';
+import { MatDialog, MatDialogRef, MAT_DIALOG_DATA } from '@angular/material';
+import { SelectActionComponent } from '../select-action/select-action.component';
+export interface DialogData {
+  animal: string;
+  name: string;
+}
 
 @Component({
   selector: 'app-withdrawal-deposit',
@@ -6,10 +13,31 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./withdrawal-deposit.component.scss']
 })
 export class WithdrawalDepositComponent implements OnInit {
-
-  constructor() { }
-
+  public WithDrawalDeposit = 100;
+  constructor(public dialog: MatDialog) { }
+  openDialog(): void {
+    const dialogRef = this.dialog.open(SelectActionComponent, {
+      width: '330px',
+      //data: {name: this.name, animal: this.animal}
+    });
+    dialogRef.afterClosed().subscribe(result => {
+      console.log('The dialog was closed');
+      //this.animal = result;
+    });
+  }
   ngOnInit() {
   }
+}
 
+export class DialogOverviewExampleDialog {
+  constructor(
+    public dialogRef: MatDialogRef<DialogOverviewExampleDialog>,
+    @Inject(MAT_DIALOG_DATA) public data: DialogData) {}
+  onNoClick(): void {
+    this.dialogRef.close();
+  }
+
+  closeDialog(){
+    this.dialogRef.close();
+  }
 }
--- src/app/petty-cash/petty-cash.routing.module.ts
+++ src/app/petty-cash/petty-cash.routing.module.ts
@@ -1,23 +1,25 @@
 import { NgModule } from '@angular/core';
 import { RouterModule } from '@angular/router';
 import { PettyCashComponent } from './petty-cash.component';
-import { AccountTypeComponent } from './components/account-type/account-type.component';
 import { FilterComponent } from './components/filter/filter.component';
 import { BalanceComponent } from './components/balance/balance.component';
 import { SettingsComponent } from './components/settings/settings.component';
 import { ExpanseCategoriesComponent } from './components/expanse-categories/expanse-categories.component';
 import { SwitchCompanyComponent } from './components/switch-company/switch-company.component';
+import { WithdrawalDepositComponent } from './components/withdrawal-deposit/withdrawal-deposit.component';
+import { SalesIncomeComponent } from './components/sales-income/sales-income.component';
 
 @NgModule({
   imports: [
     RouterModule.forChild([
         { path: '', component: PettyCashComponent, pathMatch: 'full' },
-        { path: 'users', component: AccountTypeComponent, pathMatch: 'full' },
+        { path: 'withdrawal-deposit', component: WithdrawalDepositComponent, pathMatch: 'full' },
         { path: 'filter', component: FilterComponent, pathMatch: 'full' },
         { path: 'balance', component: BalanceComponent, pathMatch: 'full' },
         { path: 'settings', component: SettingsComponent, pathMatch: 'full' },
         { path: 'expanse-categories', component: ExpanseCategoriesComponent, pathMatch: 'full' },
         { path: 'switch-company', component: SwitchCompanyComponent, pathMatch: 'full' },
+        { path: 'sales-income', component: SalesIncomeComponent, pathMatch: 'full' },
 
     ])
   ],
